80b124dae2b6401e4442562b4ee8369a3d7fcfd3
Nancy Lee <srlee713@gmail.com>
2019-08-23 10:19:48 -0600

    Add mission sample records to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,10 @@
 neil_armstrong = Astronaut.create(name: "Neil Armstrong", age: 30, job: "Commander")
 buzz_aldrin = Astronaut.create(name: "Buzz Aldrin", age: 40, job: "Engineer")
 pete_conrad = Astronaut.create(name: "Pete Conrad", age: 50, job: "Aviator")
+
+lightsail_2 = neil_armstrong.missions.create!(title: "LightSail 2", time_in_space: 2700)
+chandrayaan_2 = neil_armstrong.missions.create!(title: "Chandrayaan-2", time_in_space: 5000)
+mars_2020 = buzz_aldrin.missions.create!(title: "Mars 2020", time_in_space: 3300)
+hayabusa_2 = buzz_aldrin.missions.create!(title: "Hayabusa2", time_in_space: 2345)
+insight = pete_conrad.missions.create!(title: "InSight", time_in_space: 8000)
+osiris_rex = pete_conrad.missions.create!(title: "OSIRIS-REx", time_in_space: 1500)
