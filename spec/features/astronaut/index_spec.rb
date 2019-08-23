15c85e80133d6ae42fca980086b33664056739b6
Nancy Lee <srlee713@gmail.com>
2019-08-23 09:41:14 -0600

    Organize index spec test by including within sections for each astronaut

--- spec/features/astronaut/index_spec.rb
+++ spec/features/astronaut/index_spec.rb
@@ -6,21 +6,27 @@ RSpec.describe "the Astronaut index page" do
     @buzz_aldrin = Astronaut.create!(name: "Buzz Aldrin", age: 37, job: "Engineer")
     @pete_conrad = Astronaut.create!(name: "Pete Conrad", age: 37, job: "Aviator")
   end
-  
+
   it "should display all astronauts" do
     visit "/astronauts"
 
-    expect(page).to have_content(@neil_armstrong.name)
-    expect(page).to have_content(@neil_armstrong.age)
-    expect(page).to have_content(@neil_armstrong.job)
+    within "#astronaut-#{@neil_armstrong.id}" do
+      expect(page).to have_content(@neil_armstrong.name)
+      expect(page).to have_content(@neil_armstrong.age)
+      expect(page).to have_content(@neil_armstrong.job)
+    end
 
-    expect(page).to have_content(@buzz_aldrin.name)
-    expect(page).to have_content(@buzz_aldrin.age)
-    expect(page).to have_content(@buzz_aldrin.job)
+    within "#astronaut-#{@buzz_aldrin.id}" do
+      expect(page).to have_content(@buzz_aldrin.name)
+      expect(page).to have_content(@buzz_aldrin.age)
+      expect(page).to have_content(@buzz_aldrin.job)
+    end
 
-    expect(page).to have_content(@buzz_aldrin.name)
-    expect(page).to have_content(@buzz_aldrin.age)
-    expect(page).to have_content(@buzz_aldrin.job)
+    within "#astronaut-#{@pete_conrad.id}" do
+      expect(page).to have_content(@pete_conrad.name)
+      expect(page).to have_content(@pete_conrad.age)
+      expect(page).to have_content(@pete_conrad.job)
+    end
   end
 end
 
