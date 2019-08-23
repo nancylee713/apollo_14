d0274753fa3456396dce73d6fa2278d4cdca61c5
Nancy Lee <srlee713@gmail.com>
2019-08-23 10:01:26 -0600

    Add sample records to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+neil_armstrong = Astronaut.create(name: "Neil Armstrong", age: 30, job: "Commander")
+buzz_aldrin = Astronaut.create(name: "Buzz Aldrin", age: 40, job: "Engineer")
+pete_conrad = Astronaut.create(name: "Pete Conrad", age: 50, job: "Aviator")
